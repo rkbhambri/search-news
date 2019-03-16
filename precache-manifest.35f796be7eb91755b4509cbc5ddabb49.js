ad89342318e2ae9631641064ed498b96bfb1f039
ram <ramkrishan@kontikilabs.com>
2019-03-16 10:13:46 +0530

    Updates

---
+++ precache-manifest.35f796be7eb91755b4509cbc5ddabb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b39a92085e63f270bd2",
+    "url": "/search-news/static/js/runtime~main.654a8364.js"
+  },
+  {
+    "revision": "0a5035f35dbd1e076c31",
+    "url": "/search-news/static/js/main.c4cf1a50.chunk.js"
+  },
+  {
+    "revision": "bc1e867d8a3345970554",
+    "url": "/search-news/static/js/2.e80bc44c.chunk.js"
+  },
+  {
+    "revision": "0a5035f35dbd1e076c31",
+    "url": "/search-news/static/css/main.69d6425f.chunk.css"
+  },
+  {
+    "revision": "bc1e867d8a3345970554",
+    "url": "/search-news/static/css/2.c99df3cc.chunk.css"
+  },
+  {
+    "revision": "6ed4ee80fafba141ef61edbde9f6a249",
+    "url": "/search-news/index.html"
+  }
+];
